a71d967eef4eb155278ef12da2e1b5284ea9bb71
justin <64299580+aceus0@users.noreply.github.com>
2024-10-05 13:30:21 -0400

    reworked the code since I initially inplemented a DFS which was very wrong, now complete

--- chess.js
+++ chess.js
@@ -1,65 +1,65 @@
-// 0 will repersent squares that haven't been visited,
-// 1 will visited squares,
-// 2 will be possible moves,
-// 5 will be where the knight it presently.
-// 10 will be the goal square
-
 function knightMoves(start, end) {
-  const boardGraph = [
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-  ];
-
   const maxRow = 7;
   const maxCol = 7;
 
-  boardGraph[start[0]][start[1]] = 5;
-  boardGraph[end[0]][end[1]] = 10;
+  const knightPossibleMoves = [
+    [-2, -1],
+    [-2, 1],
+    [2, -1],
+    [2, 1],
+    [-1, -2],
+    [1, -2],
+    [-1, 2],
+    [1, 2],
+  ];
 
   const moves = (knightPos) => {
-    const knightPossibleMoves = [
-      [-2, -1],
-      [-2, 1],
-      [2, -1],
-      [2, 1],
-      [-1, -2],
-      [1, -2],
-      [-1, 2],
-      [1, 2],
-    ];
-
     const [row, col] = knightPos;
+    const validMoves = [];
 
     knightPossibleMoves.forEach(([dRow, dCol]) => {
       const newRow = row + dRow;
       const newCol = col + dCol;
 
       if (newRow >= 0 && newRow <= maxRow && newCol >= 0 && newCol <= maxCol) {
-        boardGraph[newRow][newCol] = 2;
+        validMoves.push([newRow, newCol]);
       }
     });
-
-    console.table(boardGraph);
+    return validMoves;
   };
 
-  const findPath = (knightPos, movesCount) => {
-    const [row, col] = knightPos;
+  const findPath = (start, end) => {
+    const queue = [[start, [start]]];
+    const visited = new Set();
+    visited.add(start.toString());
 
-    if (row === end[0] && col == end[1]) {
-      minMoves = Math.min(minMoves, movesCount);
-      return;
+    while (queue.length > 0) {
+      const [currentPos, path] = queue.shift();
+      const [row, col] = currentPos;
+
+      if (row === end[0] && col == end[1]) {
+        console.log(
+          `Shortest path found in ${path.length - 1} moves:`,
+          JSON.stringify(path)
+        );
+        return path;
+      }
+
+      const nextMoves = moves(currentPos);
+
+      nextMoves.forEach(([newRow, newCol]) => {
+        const newPos = [newRow, newCol];
+        if (!visited.has(newPos.toString())) {
+          visited.add(newPos.toString());
+          queue.push([newPos, [...path, newPos]]);
+        }
+      });
     }
-
-    boardGraph[row][col] = 1;
+    console.log("no path");
+    return null;
   };
 
-  moves(start);
+  findPath(start, end);
 }
 
 knightMoves([1, 1], [5, 5]);
